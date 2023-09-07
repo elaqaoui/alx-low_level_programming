c46a78c5dd5eabc66fa6b200f3f8adc5212cdafc
elaqaoui <mohamed.elaqaoui@gmail.com>
2023-09-07 14:55:43 +0200

    task4 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-int main() {
-	    puts("Programming is like building a multilingual puzzle");
-	        return 0;
+//main - Entry point
+int main(void){
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
-
